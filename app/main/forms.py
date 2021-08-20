96d9820c7091773bc748f296e41b6c227f595103
Cian <cian.omondi@student.moringaschool.com>
2021-08-20 23:45:47 +0300

    added form and email function

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,20 +1,24 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,TextAreaField,SubmitField
-# from wtforms.validators import Required
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
 
 
-# class BlogForm(FlaskForm):
+class BlogForm(FlaskForm):
 
-#  title = StringField('Blog title',validators=[Required()])
+ title = StringField('Blog title',validators=[Required()])
 
-#  author = StringField('Name',validators=[Required()])
+ author = StringField('Name',validators=[Required()])
 
-#  review = TextAreaField('Write blog here: ')
+ blog = TextAreaField('Write blog here: ')
 
-#  submit = SubmitField('Submit')
+ submit = SubmitField('Submit')
 
 
 
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('Tell us about you.',validators = [Required()])
-#     submit = SubmitField('Submit')
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment',validators=[Required()])
+    submit = SubmitField('Submit')
